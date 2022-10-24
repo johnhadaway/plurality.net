0e402089a93798c2d720544b13720322775d61e5
johnhadaway <johnhadaway8@gmail.com>
2022-10-24 17:25:28 +0300

    updated dir structure; framework and stylesheet for landing in place

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/site/assets/css/landing-page.css");
+
   return {
     dir: {
       input: "src/site",
-      output: "public"
+      output: "public",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data"
     }
   }
 }
